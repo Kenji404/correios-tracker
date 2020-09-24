fbb4a489da1250b9f2e54fd85e29faa86365243c
Kenji404 <kenji.andre@gmail.com>
2020-09-24 09:54:36 -0300

    fixed broken pt-br html entities

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const qs = require('qs');
+const iso88592 = require('iso-8859-2');
 
 const fetchData = async() => {
     const result = await axios({
         method: 'post',
         url: 'https://www2.correios.com.br/sistemas/rastreamento/resultado.cfm',
+        responseType: 'arraybuffer',
+        responseEncoding: 'binary',
         data: qs.stringify({
             acao: 'track',
             objetos: 'OK794388532BR',
             btnPesq: 'Buscar'
         }),
         headers: {
-            'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+            'content-type': 'application/x-www-form-urlencoded;charset=ISO-8859-1'
         }
     });
     return result.data;
 }
 
 const main = async() => {
-    const content = await fetchData();
+    let content = await fetchData();
+    content = iso88592.decode(content.toString('binary'));
+
     const $ = cheerio.load(content);
 
-    const title = $('table').text();
-    console.log(title);
+    let title = $('.ctrlcontent').html();
+        console.log(title);
 }
 
 main();
